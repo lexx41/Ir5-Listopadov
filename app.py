5ae6aa8f3fe0aec98b23cc1c6c30d27efd12a646
lex41 <listopadovir@gmail.com>
2026-02-24 23:21:51 +0400

    feat: add goodbye function

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 def greet(name):
     return f"Hello, {name}!"
 
+def goodbye(name):
+    return f"Goodbye, {name}!"
+
 if __name__ == "__main__":
     print(greet("Student"))
+    print(goodbye("Student"))
+
